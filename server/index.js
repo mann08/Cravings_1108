edf823e93c39d50a108832bcaedb656a2f26d80a
mann08 <mann882005@gmail.com>
2026-06-27 16:48:07 +0530

    feat: initialize server with basic structure and dependencies
    - Added package.json for server configuration with scripts and dependencies.
    - Created dbConnecton.config.js for MongoDB connection setup.
    - Added initial controller files for authentication and public routes.
    - Implemented middleware for authentication.
    - Created user model for MongoDB.
    - Set up routing for authentication and public endpoints.

---
+++ server/index.js
@@ -0,0 +1,31 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+
+import connectDB from "./src/config/dbConnecton.config.js";
+
+const app = express();
+
+app.use(express.json());
+
+// Default API
+app.get("/", (req, res) => {
+  console.log("Default Get API Hit");
+  res.json({ message: "Welcome to my first backend project" });
+});
+
+// Default error handler
+app.use((err, req, res, next) => {
+  const ErrMessage = err.message || "Internal Server Error";
+  const ErrStatusCode = err.statusCode || 500;
+
+  res.status(ErrStatusCode).json({ message: ErrMessage });
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log("Server Started on port : ", port);
+  connectDB();
+});
