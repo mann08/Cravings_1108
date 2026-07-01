c2dea991fb9dafe0eac69c9dd1db9fd4b1f1cfae
mann08 <mann882005@gmail.com>
2026-07-01 19:58:52 +0530

    feat: Add AuthContext and AuthProvider for user authentication management

---
+++ client/src/context/AuthContext.jsx
@@ -0,0 +1,30 @@
+import React, { useContext, useEffect, useState } from "react";
+
+const AuthContext = React.createContext;
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState();
+  const [isLogin, setIsLogin] = useState(false);
+
+  useEffect(() => {
+    // long method
+    // if (user) {
+    //   setIsLogin(true);
+    // } else {
+    //   setIsLogin(false);
+    // }
+    // short method
+    setIsLogin(!!user);
+  }, [user]);
+
+  const value = {
+    user,
+    setUser,
+    isLogin,
+    setIsLogin,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
+
+export const useAuth = () => useContext(AuthContext);
