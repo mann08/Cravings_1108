9455d655a9d0c02797aeacb9d9605c88df83a8af
mann08 <mann882005@gmail.com>
2026-07-02 19:27:48 +0530

    feat: Initialize user state from sessionStorage and update login state management

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -3,8 +3,10 @@ import React, { useContext, useEffect, useState } from "react";
 const AuthContext = React.createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState([]);
-  const [isLogin, setIsLogin] = useState(false);
+  const [user, setUser] = useState(
+    JSON.parse(sessionStorage.getItem("UserData")) || "",
+  );
+  const [isLogin, setIsLogin] = useState(!!user);
 
   useEffect(() => {
     // long method
