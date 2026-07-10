306b6cf2a6c24836d9fa60d301d903c3362e2c24
mann08 <mann882005@gmail.com>
2026-07-10 15:06:56 +0530

    feat: Define Contact model schema and create new model files for Customer, Restaurant, and Rider

--- server/src/models/contact.model.js
+++ server/src/models/contact.model.js
@@ -1,18 +1,33 @@
 import mongoose from "mongoose";
 
-const UserSchema = mongoose.Schema(
+const ContactSchema = mongoose.Schema(
   {
-    fullName: { type: String, required: true },
-    email: { type: String, required: true },
-    subject: { type: String, required: true },
-    phone: { type: String, required: true },
-    description: { type: String, required: true },
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    subject: {
+      type: String,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
   },
   {
     timestamps: true,
   },
 );
 
-const Contact = mongoose.model("Contact", UserSchema);
+const Contact = mongoose.model("contact", ContactSchema);
 
 export default Contact;
