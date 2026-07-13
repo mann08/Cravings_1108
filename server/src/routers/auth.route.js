82220772d12912810c18fbab028594ec6426a58b
mann08 <mann882005@gmail.com>
2026-07-13 15:18:22 +0530

    feat: Add routes for OTP sending, verification, and password reset

--- server/src/routers/auth.route.js
+++ server/src/routers/auth.route.js
@@ -1,14 +1,22 @@
 import express from "express";
 import {
-  RegisterUser,
   LoginUser,
   LogoutUser,
-} from "../controllers/auth.controller.js";
+  RegisterUser,
+  SendOtp,
+  VerifyOtp,
+  ResetPassword,
+} from "../controller/auth.controller.js";
+import { OTPAuthProtect } from "../middleware/auth.middelware.js";
 
 const router = express.Router();
 
 router.post("/register", RegisterUser);
 router.post("/login", LoginUser);
 router.get("/logout", LogoutUser);
+
+router.post("/send-otp", SendOtp);
+router.post("/verify-otp", VerifyOtp);
+router.post("/reset-password", OTPAuthProtect, ResetPassword);
 
 export default router;
