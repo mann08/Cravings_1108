d40a4c40e3136816961f077978d6ae158ac0dde3
mann08 <mann882005@gmail.com>
2026-08-07 22:43:23 +0530

    feat: remove console log statements from database connection for cleaner code

--- server/src/config/dbConnection.config.js
+++ server/src/config/dbConnection.config.js
@@ -3,11 +3,9 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_DB_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error("MongoDB connection error:", error.message);
     process.exit(1);
   }
 };
-
+  
 export default connectDB;
