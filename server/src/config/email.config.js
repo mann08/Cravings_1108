691da1afa5e6a8c8662446ccdfa338305db7d3a6
mann08 <mann882005@gmail.com>
2026-08-07 22:44:38 +0530

    refactor: remove unnecessary whitespace in email configuration

--- server/src/config/email.config.js
+++ server/src/config/email.config.js
@@ -4,9 +4,6 @@ import nodemailer from "nodemailer";
 
 const sendEmail = async (to, subject, message) => {
   try {
-
-
-
     const transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
@@ -14,16 +11,12 @@ const sendEmail = async (to, subject, message) => {
         pass: process.env.GMAIL_PASSCODE,
       },
     });
-
-
     const mailOption = {
       from: process.env.GMAIL_USERNAME,
       to,
       subject,
       html: message,
     };
-
-
     const res = await transporter.sendMail(mailOption);
   } catch (error) {
     throw error;
